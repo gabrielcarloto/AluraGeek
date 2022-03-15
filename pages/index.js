b0e2ae7df43ca0ed2a26af593d7d6a5a7906035f
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-14 21:54:10 -0300

    feat: add logo on header
    #2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,43 @@
 import Head from 'next/head'
+import { styled } from '@stitches/react'
 
 export default function Home() {
+  function Header() {
+    const Header = styled('header', {
+      display: 'flex',
+      width: '100vw',
+      height: '115px',
+      backgroundColor: '#ffffff',
+
+      '.content': {
+        margin: '0 auto',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+        'img': {
+          width: '100px',
+          height: '28px',
+          
+          '@media (min-width: 768px)': {
+            width: '176px',
+            height: '50px',
+          }
+        }
+      }
+    })
+    
+    return (
+      <Header>
+        <div className="content">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/Logo.svg" alt="logo" />
+        </div>
+      </Header>
+    )
+  }
+
   return (
     <>
       <Head>
@@ -8,12 +45,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>AluraGeek</h1>
-        <p>
-          Aprenda a programar com o <a href="https://www.alura.com.br/">Alura</a>
-        </p>
-      </main>
+      <Header />
     </>
   )
 }
