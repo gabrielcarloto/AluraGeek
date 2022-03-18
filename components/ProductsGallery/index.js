9c2f241b9aef067fd062e7aa678cef817ffd93d1
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-17 21:13:10 -0300

    fix: remove 'prefetch' attribute from products links

--- components/ProductsGallery/index.js
+++ components/ProductsGallery/index.js
@@ -13,7 +13,7 @@ function ProductsGallery({ title, products }) {
         <Grid>
           {products.map(product => {
             return (
-              <Link passHref prefetch href="/products/[id]" as={`/products/${product.id}`} key={product.id}>
+              <Link passHref href="/products/[id]" as={`/products/${product.id}`} key={product.id}>
                 <Product>
                   <div className="product-image">
                     <Image src={product.image} alt={product.alt} width={500} height={500} objectFit="cover" />
