89f5ecb774a761df8fadba5d5d80b6fe0578e9a7
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-21 12:08:30 -0300

    feat: add animation to search input on mobile devices

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image';
 import { css } from '@stitches/react';
@@ -6,6 +7,9 @@ import Container from '../utils/Container';
 import Input from '../Inputs/Input';
 
 function Header({ loginBtn }) {
+  const headerSearchMobile = React.useRef(null);
+  const headerForm = React.useRef(null);
+
   const Header = css({
     display: 'flex',
     width: '100vw',
@@ -20,90 +24,156 @@ function Header({ loginBtn }) {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
-      
-      '.header-left-content': {
+  
+      '.header-logo': {
+        width: '100px',
+        height: '28px',
+        cursor: 'pointer',
+
+        '@media (min-width: 768px)': {
+          width: '176px',
+          height: '50px',
+        },
+      },
+
+      '.header-form': {
+        height: '72px',
+        width: '100%',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between',
-  
-        '.header-logo': {
-          width: '100px',
-          height: '28px',
+        justifyContent: 'space-around',
+        position: 'absolute',
+        top: '-72px',
+        left: '0',
+        transition: 'all 200ms ease-in-out',
+        
+        '.header-search': {
+          width: '60%',
+        },
+
+        '.header-search-icon': {
+          width: '18px',
+          height: '18px',
+          display: 'block',
+          marginLeft: '-28vw',
+          background: 'url(/search.svg) no-repeat center',
+          backgroundColor: 'transparent',
+          color: 'transparent',
+          border: 'none',
+          outline: 'none',
           cursor: 'pointer',
-  
+          transition: 'all 200ms ease-in-out',
+
           '@media (min-width: 768px)': {
-            width: '176px',
-            height: '50px',
-            marginRight: '48px',
+            marginLeft: '-35px',
+          },
+
+          '&:focus': {
+            transform: 'scale(1.2)',
           },
         },
-  
-        'form': {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
+
+        '.header-close-icon': {
+          width: '16px',
         },
-  
-        '.header-search': {
-          display: 'none',
-          
-          '@media (min-width: 768px)': {
+
+        '&.active': {
+          top: '0',
+        },
+
+        '@media (min-width: 768px)': {
+          display: 'flex',
+          width: 'auto',
+          position: 'static',
+          alignItems: 'center',
+          justifyContent: 'center',
+
+          '.header-search': {
             display: 'block',
             width: 'clamp(300px, 30vw, 400px)',
             maxWidth: '400px',
           },
         },
       },
-  
-      '.header-search-icon--desktop': {
+
+      '.desktop': {
         display: 'none',
       },
   
       '@media (min-width: 768px)': {
-        '.header-search-icon--mobile': {
+        '.mobile': {
           display: 'none',
         },
   
-        '.header-search-icon--desktop': {
+        '.desktop': {
           display: 'block',
-          marginLeft: '-35px',
-          backgroundColor: 'transparent',
-          color: 'transparent',
-          border: 'none',
-          outline: 'none',
-          cursor: 'pointer',
-          background: 'url(/search.svg) no-repeat center',
-          width: '18px',
-          height: '18px',
-          transition: 'all 200ms ease-in-out',
-  
-          '&:focus': {
-            transform: 'scale(1.2)',
-          },
         },
       },
     },
   
     '.header-login': {
-      width: 'clamp(133px, 15vw, 182px)'
-    }
+      width: '100px',
+
+      '@media (min-width: 768px)': {
+        width: '176px',
+      },
+    },
+
+    '.header-search--mobile': {
+      width: '100vw',
+      height: '72px',
+      position: 'absolute',
+      top: '-72px',
+      left: '0',
+      backgroundColor: '#FFFFFF',
+      transition: 'all 200ms ease-in-out',
+
+      '&.active': {
+        top: '0',
+      },
+    },
   })
 
   return (
     <header className={Header()}>
       <Container className="header-container">
-        <div className="header-left-content">
-          <Link passHref href="/" >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img className="header-logo" src="/Logo.svg" alt="logo da empresa alurageek" />
-          </Link>
-          <form>
-            <label className="scr-only" htmlFor="header-search">O que deseja encontrar?</label>
-            <Input className="header-search" id="header-search" color="grey" type="text" placeholder="O que deseja encontrar?" />
-            <button className="header-search-icon--desktop" type="submit">
-              Pesquisar
-            </button>
-          </form>
+        <div className="header-search--mobile" ref={headerSearchMobile} />
+        <Link passHref href="/" >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img className="header-logo" src="/Logo.svg" alt="logo da empresa alurageek" />
+        </Link>
+        <form className="header-form" ref={headerForm}>
+          <label className="scr-only" htmlFor="header-search">O que deseja encontrar?</label>
+          <Input 
+            className="header-search"
+            id="header-search"
+            color="grey"
+            type="text"
+            placeholder="O que deseja encontrar?"
+          />
+          <button className="header-search-icon desktop" type="submit">
+            Pesquisar
+          </button>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img 
+            className="header-close-icon mobile"
+            src="/close.svg"
+            alt="fechar"
+            onClick={() => {
+              headerSearchMobile.current.classList.remove('active');
+              headerForm.current.classList.remove('active');
+            }}
+          />
+        </form>
+        <div 
+          className="header-search-icon mobile"
+          onClick={() => {
+            headerSearchMobile.current.classList.add('active');
+            headerForm.current.classList.add('active');
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/search.svg" alt="ícone de pesquisa" />
         </div>
         { loginBtn && (
           <Link passHref href="/login">
@@ -112,9 +182,6 @@ function Header({ loginBtn }) {
             </Button>
           </Link>
         )}
-        <div className="header-search-icon--mobile">
-          <Image src="/search.svg" width="17" height="17" alt="ícone de busca" />
-        </div>
       </Container>
     </header>
   )
