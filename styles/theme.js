61e4e85dd0fdde120290dc4cff3ac8e5af7dd46d
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-25 10:43:04 -0300

    feat: create theme tokens

---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, css } = createStitches({
+  theme: {
+    colors: {
+      primary: "#2A7AE4",
+      secondary: "#EAF2FD",
+      background: "#F5F5F5",
+      lightBackground: "#FFFFFF",
+      text: "#464646",
+      lightText: "#A2A2A2",
+      icon: "#A2A2A2",
+      lightIcon: "#C8C8C8",
+      border: "#C8C8C8",
+      error: "#FF5252",
+      success: "#4CAF50",
+      warning: "#FFC107",
+      white: "#FFFFFF",
+    },
+  }
+});
