b5eb9bec7fe8bf90e8385a5ae4c58d551d7b0700
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-26 15:03:01 -0300

    feat: create Fill component

---
+++ components/utils/Fill.js
@@ -0,0 +1,20 @@
+import { styled } from "../../styles/theme";
+
+const Fill = styled('div', {
+  width: "100%",
+  height: '50vh',
+
+  '@media (min-width: 768px)': {
+    height: 'calc(100vh - 483px - 115px)',
+  },
+
+  '@media (min-width: 1024px)': {
+    height: 'calc(100vh - 521px - 115px)',
+  },
+
+  '@media (min-width: 1440px)': {
+    height: 'calc(100vh - 464px - 115px)',
+  },
+});
+
+export default Fill;
