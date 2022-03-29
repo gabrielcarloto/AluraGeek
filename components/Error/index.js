d5d749710e8f21f7a8f840fabadb7cb172ebb7d7
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-29 19:13:04 -0300

    feat: add initial animation to Error component

--- components/Error/index.js
+++ components/Error/index.js
@@ -92,6 +92,7 @@ function Error({ queryError, error, state, setState, close }) {;
   return (
     <>
       <motion.div
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.2 }}
