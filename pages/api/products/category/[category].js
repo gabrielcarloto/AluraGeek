d409d3fbc76e12a64a7a6b941e6417e5fe3aedd0
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-30 15:52:30 -0300

    feat: create products categories api

---
+++ pages/api/products/category/[category].js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function productCategoryHandler(req, res) {
+  const {
+    query: { category },
+    method,
+  } = req;
+
+  const products = await prisma.product.findMany({
+    where: { category },
+  });
+
+  if (method === 'GET') {
+    if (products.length === 0) {
+      res.status(404).json({ error: "No products found" });
+      return;
+    }
+
+    res.status(200).json(products);
+  } else {
+    res.statusCode = 405;
+    res.setHeader("Allow", "GET");
+    res.end("Method Not Allowed");
+  };
+};
