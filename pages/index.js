40660fa683b8707ef8ef681e8d28c386f11f8814
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-31 11:49:41 -0300

    refactor: change products title to lower case on the home page

--- pages/index.js
+++ pages/index.js
@@ -57,11 +57,11 @@ export default function Home() {
 
       <Banner />
       <Spacer responsive={1} />
-      <ProductsGallery title="Star Wars" isSmall={isSmall} products={starWars.slice(0, isSmall ? 4 : 6)} />
+      <ProductsGallery title="star wars" isSmall={isSmall} products={starWars.slice(0, isSmall ? 4 : 6)} />
       <Spacer responsive={2} />
-      <ProductsGallery title="Consoles" isSmall={isSmall} products={consoles.slice(0, isSmall ? 4 : 6)} />
+      <ProductsGallery title="consoles" isSmall={isSmall} products={consoles.slice(0, isSmall ? 4 : 6)} />
       <Spacer responsive={2} />
-      <ProductsGallery title="Outros" isSmall={isSmall} products={others.slice(0, isSmall ? 4 : 6)} />
+      <ProductsGallery title="outros" isSmall={isSmall} products={others.slice(0, isSmall ? 4 : 6)} />
       <Spacer responsive={1} />
     </>
   );
