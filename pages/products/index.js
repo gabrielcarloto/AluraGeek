77639d07df0b9d63cf07a7fdd767dea69dd4711e
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-03-31 11:41:18 -0300

    feat: add error message to all products page

--- pages/products/index.js
+++ pages/products/index.js
@@ -5,11 +5,21 @@ import ProductSkeleton from "../../components/Product/ProductSkeleton";
 import Container from "../../components/utils/Container";
 import Grid from "../../components/utils/Grid";
 import Spacer from "../../components/utils/Spacer";
+import Fill from "../../components/utils/Fill";
+import Error from "../../components/Error";
 
 export default function Products() {
   const fetcher = (url) => fetch(url).then((r) => r.json());
-  const data = useSWR("/api/products", fetcher);
-  const products = data.data || [];
+  const { data, error } = useSWR("/api/products", fetcher);
+
+  if (error) return (
+    <>
+      <Fill />
+      <Error error="Ocorreu um erro. Atualize a página" />
+    </>
+  );
+
+  const products = data || [];
 
   return (
     <>
