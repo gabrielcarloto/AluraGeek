a83aa8fcbf54ef5b2e234f686204d76659bf0b71
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-04-09 15:17:54 -0300

    feat: add PUT method

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -20,9 +20,25 @@ export default async function handler(req, res) {
     const product = await prisma.product.create({ data: productData });
 
     res.status(201).json(product);
+  } else if (req.method === 'PUT') {
+    const { body, headers } = req;
+
+    if (headers['authorization'] !== password) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    };
+
+    const productData = JSON.parse(body);
+
+    const product = await prisma.product.update({
+      where: { id: productData.id },
+      data: productData
+    });
+
+    res.status(200).json(product);
   } else {
     res.statusCode = 405;
-    res.setHeader('Allow', 'GET', 'POST');
+    res.setHeader('Allow', 'GET', 'POST', 'PUT');
     res.end('Method Not Allowed');
   }
 }
