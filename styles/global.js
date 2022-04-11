293d8e137d2f52a372589fbd31e2a15765f28e65
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-04-11 18:54:03 -0300

    fix: remove display inline-block from links

--- styles/global.js
+++ styles/global.js
@@ -1,86 +1,85 @@
 import { globalCss } from "./theme";
-import "@fontsource/raleway/400.css"
-import "@fontsource/raleway/500.css"
-import "@fontsource/raleway/600.css"
-import "@fontsource/raleway/700.css"
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/600.css";
+import "@fontsource/raleway/700.css";
 
 const globalStyles = globalCss({
-  '*': {
-    boxSizing: 'border-box',
+  "*": {
+    boxSizing: "border-box",
     margin: 0,
     padding: 0,
-    maxWidth: '100%',
+    maxWidth: "100%",
   },
 
-  'body': {
-    position: 'relative',
-    fontFamily: 'Raleway',
-    fontWeight: '400',
-    color: '$text',
+  body: {
+    position: "relative",
+    fontFamily: "Raleway",
+    fontWeight: "400",
+    color: "$text",
   },
 
-  'html, body': {
-    height: '100vh',
-    width: '100vw',
-    backgroundColor: '$background',
-    overflowX: 'clip',
+  "html, body": {
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "$background",
+    overflowX: "clip",
   },
 
-  'a': {
-    textDecoration: 'none',
-    color: '$text',
-    display: 'inline-block',
-    position: 'relative',
-    cursor: 'pointer',
-    transition: 'color 200ms ease-in-out',
-    
-    '&:visited': {
-      color: 'inherit',
+  a: {
+    textDecoration: "none",
+    color: "$text",
+    position: "relative",
+    cursor: "pointer",
+    transition: "color 200ms ease-in-out",
+
+    "&:visited": {
+      color: "inherit",
     },
-    
-    '@media (hover: hover) and (pointer: fine)': {
-      '&:hover': {
-        color: '$primary',
+
+    "@media (hover: hover) and (pointer: fine)": {
+      "&:hover": {
+        color: "$primary",
       },
-      
-      '&::after': {
+
+      "&::after": {
         content: '""',
-        display: 'block',
-        position: 'absolute',
-        width: '0%',
-        height: '2.5px',
-        backgroundColor: '$primary',
-        transition: 'all 200ms ease-in-out',
+        display: "block",
+        position: "absolute",
+        width: "0%",
+        height: "2.5px",
+        backgroundColor: "$primary",
+        transition: "all 200ms ease-in-out",
       },
-  
-      '&:hover::after': {
-        width: '100%',
+
+      "&:hover::after": {
+        width: "100%",
       },
     },
   },
 
-  '.scr-only': {
-    position: 'absolute',
-    height: '1px',
-    width: '1px',
-    clip: 'rect(1px 1px 1px 1px)',
-    clipPath: 'polygon(0px 0px, 0px 0px, 0px 0px)',
-    '-webkit-clip-path': 'polygon(0px 0px, 0px 0px, 0px 0px)',
-    overflow: 'hidden !important',
+  ".scr-only": {
+    position: "absolute",
+    height: "1px",
+    width: "1px",
+    clip: "rect(1px 1px 1px 1px)",
+    clipPath: "polygon(0px 0px, 0px 0px, 0px 0px)",
+    "-webkit-clip-path": "polygon(0px 0px, 0px 0px, 0px 0px)",
+    overflow: "hidden !important",
   },
 
-  '#nprogress': {
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    right: '0',
-    zIndex: '9999',
-    pointerEvents: 'none',
+  "#nprogress": {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    zIndex: "9999",
+    pointerEvents: "none",
   },
 
-  '#nprogress .bar': {
-    background: '$primary',
-    height: '3px',
+  "#nprogress .bar": {
+    background: "$primary",
+    height: "3px",
   },
 });
 
