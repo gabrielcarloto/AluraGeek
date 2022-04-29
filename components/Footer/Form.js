9f8eb0c209868cd7b9ef2342d099ac45a319795a
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-04-29 20:10:27 -0300

    refactor: add LabeledInput to Footer

--- components/Footer/Form.js
+++ components/Footer/Form.js
@@ -1,120 +1,55 @@
-import React from "react";
-import { css } from "../../styles/theme";
-import Spacer from "../utils/Spacer";
-import Button from "../Button/index";
-import Input from "../Inputs/Input";
-import Textarea from "../Inputs/Textarea";
-import FloatLabel from "../Inputs/FloatLabel";
+import React from 'react';
+import { css } from '../../styles/theme';
+import Spacer from '../utils/Spacer';
+import Button from '../Button/index';
+import LabeledInput from '../Inputs/LabeledInput';
 
 function Form() {
-  const [nameValue, setNameValue] = React.useState("");
-  const [messageValue, setMessageValue] = React.useState("");
-  const nameLabelBg = React.useRef(null);
-  const messageLabelBg = React.useRef(null);
+  const [nameValue, setNameValue] = React.useState('');
+  const [messageValue, setMessageValue] = React.useState('');
 
   const Form = css({
-    width: "100%",
-    gridColumn: "1 / -1",
+    width: '100%',
+    gridColumn: '1 / -1',
 
-    "@media (min-width: 768px)": {
-      gridColumn: "6 / -1",
-      alignSelf: "flex-start",
+    '@media (min-width: 768px)': {
+      gridColumn: '6 / -1',
+      alignSelf: 'flex-start',
     },
 
-    "@media (min-width: 1024px)": {
-      gridColumn: "7 / -1",
-      gridRow: "-2 / 3",
+    '@media (min-width: 1024px)': {
+      gridColumn: '7 / -1',
+      gridRow: '-2 / 3',
     },
 
-    ".form-title": {
-      fontSize: "16px",
-      fontWeight: "700",
-      color: "$text",
-      marginBottom: "8px",
-    },
-
-    label: {
-      fontSize: "12px",
-      fontWeight: "400",
-
-      "&.active": {
-        transform: "translate(12px, 6px) scale(1)",
-      },
-
-      "@media (min-width: 768px)": {
-        label: {
-          "&.active": {
-            transform: "translate(11px, 6px) scale(1)",
-          },
-        },
-      },
-    },
-
-    ".name-label": {
-      transform: "translate(14px, 10px) scale(1.2)",
-
-      "@media (min-width: 768px)": {
-        transform: "translate(15px, 10px) scale(1.2)",
-      },
-    },
-
-    ".message-label": {
-      transform: "translate(24px, 10px) scale(1.2)",
+    '.form-title': {
+      fontSize: '16px',
+      fontWeight: '700',
+      color: '$text',
+      marginBottom: '8px',
     },
   });
 
   return (
     <form className={Form()} onSubmit={(e) => e.preventDefault()}>
       <h3 className="form-title">Fale conosco</h3>
-      <div className={FloatLabel()}>
-        <div
-          className={
-            "label-background focus" + (nameValue == "" ? "" : " active")
-          }
-          ref={nameLabelBg}
-        />
-        <Input
-          className="form-input name"
-          id="name"
-          type="text"
-          label="true"
-          required
-          maxLength="40"
-          onChange={(event) => setNameValue(event.target.value)}
-          onFocus={() => nameLabelBg.current.classList.add("focus")}
-          onBlur={() => nameLabelBg.current.classList.remove("focus")}
-        />
-        <label
-          className={"name-label" + (nameValue !== "" ? " active" : "")}
-          htmlFor="name"
-        >
-          Nome
-        </label>
-      </div>
+      <LabeledInput
+        type="text"
+        label="Nome"
+        required
+        maxLength="40"
+        inputValue={nameValue}
+        setInputValue={setNameValue}
+      />
       <Spacer y={16} />
-      <div className={FloatLabel()}>
-        <div
-          className={
-            "label-background focus" + (messageValue == "" ? "" : " active")
-          }
-          ref={messageLabelBg}
-        />
-        <Textarea
-          className="form-input message"
-          id="message"
-          required
-          maxLength="120"
-          onChange={(e) => setMessageValue(e.target.value)}
-          onFocus={() => messageLabelBg.current.classList.add("focus")}
-          onBlur={() => messageLabelBg.current.classList.remove("focus")}
-        />
-        <label
-          className={"message-label" + (messageValue !== "" ? " active" : "")}
-          htmlFor="message"
-        >
-          Escreva sua mensagem
-        </label>
-      </div>
+      <LabeledInput
+        textarea
+        label="Escreva sua mensagem"
+        required
+        maxLength="120"
+        inputValue={messageValue}
+        setInputValue={setMessageValue}
+      />
       <Spacer y={8} />
       <Button className="form-btn" type="submit" color="primary">
         Enviar mensagem
