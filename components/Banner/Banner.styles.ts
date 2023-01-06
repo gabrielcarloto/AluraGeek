3f3027d62b0f14206acc4fc374c8a8cd14c4f48f
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-06 17:08:09 -0300

    refactor: migrate Banner to typescript

---
+++ components/Banner/Banner.styles.ts
@@ -0,0 +1,68 @@
+import { css } from '@stitches/react';
+
+export const BannerStyles = css({
+  width: '100vw',
+  height: '100vh',
+  maxHeight: '352px',
+  background:
+    'linear-gradient(180deg, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, .8) 100%), url(/banner.jpg) no-repeat center top / 200% fixed',
+
+  '@media (min-width: 768px)': {
+    backgroundSize: '100%',
+  },
+
+  '@media (min-width: 1024px)': {
+    backgroundSize: 'cover',
+    backgroundPosition: 'center bottom',
+  },
+
+  '@media (min-width: 1440px)': {
+    maxHeight: '400px',
+    backgroundPosition: 'center 115%',
+  },
+
+  '.banner-container': {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+
+    '.banner-title': {
+      fontSize: '22px',
+      fontWeight: '700',
+      lineHeight: '26px',
+      color: '$white',
+      marginTop: '0',
+      marginBottom: '8px',
+
+      '@media (min-width: 768px)': {
+        fontSize: '52px',
+        lineHeight: '60px',
+        marginBottom: '16px',
+      },
+    },
+
+    '.banner-subtitle': {
+      fontSize: '14px',
+      fontWeight: '600',
+      lineHeight: '16px',
+      color: '$white',
+      marginTop: '0',
+      marginBottom: '8px',
+
+      '@media (min-width: 768px)': {
+        fontSize: '22px',
+        lineHeight: '26px',
+        marginBottom: '16px',
+      },
+    },
+
+    '.banner-btn': {
+      width: 'clamp(119px, 10vw, 130px)',
+      marginBottom: '16px',
+
+      '@media (min-width: 768px)': {
+        marginBottom: '32px',
+      },
+    },
+  },
+});
--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,78 +1,11 @@
-import Link from "next/link";
-import { css } from "@stitches/react";
-import Button from "../Button/index";
-import Container from "../utils/Container";
+import Link from 'next/link';
+import Button from '../Button/index';
+import Container from '../utils/Container';
+import { BannerStyles } from './Banner.styles';
 
 function Banner() {
-  const Banner = css({
-    width: "100vw",
-    height: "100vh",
-    maxHeight: "352px",
-    background:
-      "linear-gradient(180deg, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, .8) 100%), url(/banner.jpg) no-repeat center top / 200% fixed",
-
-    "@media (min-width: 768px)": {
-      backgroundSize: "100%",
-    },
-
-    "@media (min-width: 1024px)": {
-      backgroundSize: "cover",
-      backgroundPosition: "center bottom",
-    },
-
-    "@media (min-width: 1440px)": {
-      maxHeight: "400px",
-      backgroundPosition: "center 115%",
-    },
-
-    ".banner-container": {
-      display: "flex",
-      justifyContent: "flex-end",
-      flexDirection: "column",
-
-      ".banner-title": {
-        fontSize: "22px",
-        fontWeight: "700",
-        lineHeight: "26px",
-        color: "$white",
-        marginTop: "0",
-        marginBottom: "8px",
-
-        "@media (min-width: 768px)": {
-          fontSize: "52px",
-          lineHeight: "60px",
-          marginBottom: "16px",
-        },
-      },
-
-      ".banner-subtitle": {
-        fontSize: "14px",
-        fontWeight: "600",
-        lineHeight: "16px",
-        color: "$white",
-        marginTop: "0",
-        marginBottom: "8px",
-
-        "@media (min-width: 768px)": {
-          fontSize: "22px",
-          lineHeight: "26px",
-          marginBottom: "16px",
-        },
-      },
-
-      ".banner-btn": {
-        width: "clamp(119px, 10vw, 130px)",
-        marginBottom: "16px",
-
-        "@media (min-width: 768px)": {
-          marginBottom: "32px",
-        },
-      },
-    },
-  });
-
   return (
-    <section className={Banner()}>
+    <section className={BannerStyles()}>
       <Container className="banner-container">
         <h1 className="banner-title">Dezembro Promocional</h1>
         <h2 className="banner-subtitle">
