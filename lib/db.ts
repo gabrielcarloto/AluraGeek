eb5e27ba8665405293cb4ee64b95d0702ffd6185
Gabriel Carloto <gabrielcarloto@tutanota.com>
2026-08-03 15:12:24 -0300

    chore: update prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-export const prisma = global.prisma || new PrismaClient();
+import { PrismaClient } from './generated/prisma';
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+const adapter = new PrismaPg({
+  connectionString: process.env['DATABASE_URL'],
+});
+
+export const prisma = new PrismaClient({ adapter });
